7e04375d757a606c3e54bba6d2e5b6955be35147
DJMF2015 <djm.fulton@gmail.com>
2018-10-17 19:22:20 +0100

    add day3 homework on hashes, looping and arrays

--- homework_day3/homework/arrays_hashes_quiz_start_point/exercise_b_users.rb
+++ homework_day3/homework/arrays_hashes_quiz_start_point/exercise_b_users.rb
@@ -4,42 +4,42 @@ users = {
     :lottery_numbers => [6, 12, 49, 33, 45, 20],
     :home_town => "Stirling",
     :pets => [
-    {
-      :name => "fluffy",
-      :species => "cat"
-    },
-    {
-      :name => "fido",
-      :species => "dog"
-    },
-    {
-      :name => "spike",
-      :species => "dog"
-    }
-  ]
+      {
+        :name => "fluffy",
+        :species => "cat"
+      },
+      {
+        :name => "fido",
+        :species => "dog"
+      },
+      {
+        :name => "spike",
+        :species => "dog"
+      }
+    ]
   },
   "Erik" => {
     :twitter => "eriksf",
     :lottery_numbers => [18, 34, 8, 11, 24],
     :home_town => "Linlithgow",
     :pets => [
-    {
-      :name => "nemo",
-      :species => "fish"
-    },
-    {
-      :name => "kevin",
-      :species => "fish"
-    },
-    {
-      :name => "spike",
-      :species => "dog"
-    },
-    {
-      :name => "rupert",
-      :species => "parrot"
-    }
-  ]
+      {
+        :name => "nemo",
+        :species => "fish"
+      },
+      {
+        :name => "kevin",
+        :species => "fish"
+      },
+      {
+        :name => "spike",
+        :species => "dog"
+      },
+      {
+        :name => "rupert",
+        :species => "parrot"
+      }
+    ]
   },
   "Avril" => {
     :twitter => "bridgpally",
@@ -55,12 +55,48 @@ users = {
 }
 
 # 1. Get Jonathan's Twitter handle (i.e. the string `"jonnyt"`)
+p users["Jonathan"][:twitter]
 # 2. Get Erik's hometown
+p users["Erik"][:home_town]
 # 3. Get the array of Erik's lottery numbers
+p users["Erik"][:lottery_numbers]
 # 4. Get the type of Avril's pet Monty
+p users["Avril"][:pets][0][:species]
 # 5. Get the smallest of Erik's lottery numbers
+p users["Erik"][:lottery_numbers].min()
 # 6. Return an array of Avril's lottery numbers that are even
+result = []
+for numb in users["Avril"][:lottery_numbers]
+  result.push(numb) if numb.even?
+end
+p result
+
 # 7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
+p users["Erik"][:lottery_numbers].unshift(7)
+# p users["Erik"][:lottery_numbers]
+
 # 8. Change Erik's hometown to Edinburgh
-# 9. Add a pet dog to Erik called "Fluffy"
+p users["Erik"][:home_town] = "Edinburgh"
+
+# 9. Add a pet dog in :pets array to Erik called "Fluffy"
+pet =
+{
+  :name => "Fluffy",
+  :species => "dog"
+}
+p users["Erik"][:pets] << pet
+
 # 10. Add another person to the users hash
+another_person  = {
+  :twitter => "dave",
+  :lottery_numbers => [6, 14, 35],
+  :home_town => "edinburgh",
+  :pets => [
+    {
+      :name => "dumpy",
+      :species => "dog"
+    }
+  ]
+}
+users["David"] = another_person
+p users
--- homework_day3/homework/arrays_hashes_quiz_start_point/exercise_c_uk.rb
+++ homework_day3/homework/arrays_hashes_quiz_start_point/exercise_c_uk.rb
@@ -17,6 +17,26 @@ united_kingdom = [
 ]
 
 # 1. Change the capital of Wales from `"Swansea"` to `"Cardiff"`.
+p united_kingdom[1][:capital] = "Cardiff"
+
 # 2. Create a Hash for Northern Ireland and add it to the `united_kingdom` array (The capital is Belfast, and the population is 1,811,000).
+country =   {
+    name: "Northern Island",
+    population: 1811000,
+    capital: "Belfast"
+  }
+united_kingdom.push(country)
+# p united_kingdom
+
 # 3. Use a loop to print the names of all the countries in the UK.
+for names in united_kingdom
+p names[:name]
+end
+
 # 4. Use a loop to find the total population of the UK.
+total_pop = 0
+for pop in united_kingdom
+  total_pop += pop[:population]
+end
+p  total_pop
+#total_pop = 63179000

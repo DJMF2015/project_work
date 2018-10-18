767e1d062b8c3cc4dcc03ba6a021f7c01889ee88
DJMF2015 <djm.fulton@gmail.com>
2018-10-18 16:04:56 +0100

    add friends lab

---
+++ day04_friends_lab/friends.rb
@@ -0,0 +1,50 @@
+def get_name(person)
+  return person[:name]
+end
+
+def get_tv_show(tv_show)
+  return tv_show[:favourites][:tv_show]
+end
+
+def get_snack(person, snack)
+    return snack  == person[:favourites][:snacks]
+end
+
+def add_new_friend(person, friend_name)
+  return person[:friends].push(friend_name)
+end
+
+def unlike_friend(person, friend_name)
+  person[:friends].delete(friend_name)
+  return person[:friends]
+end
+# 6.
+def total_of_everyones_money(everyone)
+  total_money = 0
+  for persons_money in everyone
+    total_money += persons_money[:monies]
+  end
+  return total_money
+end
+
+# 7
+
+# 8.
+def all_snacks(everyone)
+ snack = Array.new()
+ for people in everyone
+   snack.concat(people[:favourites][:snacks])
+ end
+ return snack
+end
+
+#9
+def people_with_no_friends(everyone)
+  friendless_people = Array.new()
+  for people in everyone
+    if people[:friends].length == 0
+      friendless_people.push(people)
+    end
+  end
+  return friendless_people
+end

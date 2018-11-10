3aaf6ec9c15eaac7962d4747a6e5567b3d733b98
DJMF2015 <djm.fulton@gmail.com>
2018-11-10 18:27:36 +0000

    add create member view

--- week4/sinatra_app/controllers/members_controller.rb
+++ week4/sinatra_app/controllers/members_controller.rb
@@ -7,28 +7,46 @@ require_relative( '../models/booking.rb' )
 also_reload( '../models/*' )
 
 
-# get '/' do
-#   erb(:index)
-# end
+get '/' do
+  erb(:index)
+end
 
+#FIND ALL
 get '/members' do
   @members = Member.all
   erb(:"members/index")
 end
 
-
-get '/members/:id' do
-  @member = Member.find(params['id'])
-  erb( :"members/show" )
+#CREATE
+get '/members/new' do
+  erb(:"members/new")
 end
 
-# delete
-# post '/members/:id/delete' do
-#   member = Member.find(params['id'])
-#   member.delete
-#   redirect to  '/members'
+#SAVE
+post '/members' do
+  Member.new(params).save
+  redirect to '/members'
+end
+
+# get '/members/:id' do
+#   @member = Member.find(params['id'])
+#   erb( :"members/show" )
 # end
 
+#FIND BY ID
+get '/members/:id/edit' do
+  @member = Member.find(params['id'])
+  erb( :"members/edit" )
+end
+
+#MODIFY/UPDATE
+post '/members/:id' do
+  member = Member.new(params)
+  member.update
+  redirect to "/members/#{params['id']}"
+end
+
+#DELETE
 post '/members/:id/delete' do
   Member.delete(params[:id])
   redirect to("/members")

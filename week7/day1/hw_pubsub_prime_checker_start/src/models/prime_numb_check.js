05d600e1ac55fce0399b086a084998045c4661bd
DJMF2015 <djm.fulton@gmail.com>
2018-11-27 09:36:55 +0000

    add fix to prime webpack

--- week7/day1/hw_pubsub_prime_checker_start/src/models/prime_numb_check.js
+++ week7/day1/hw_pubsub_prime_checker_start/src/models/prime_numb_check.js
@@ -1,8 +1,8 @@
-// models/priem_number_check.js
+// models/prime_number_check.js
 //responsible for the handling of the business logic of app and rendering of page
 const PubSub = require('../helpers/pub_sub.js');
 
-const PrimeCheck = new function (){
+const PrimeCheck = function (){
 
 };
 
@@ -17,14 +17,14 @@ PrimeCheck.prototype.bindEvents = function (){
 
 PrimeCheck.prototype.primeChecking = function (prime){
   if (prime <= 1){
-    return 'is not a prime numer';
+    return 'not prime';
   }
   for (let i = 2; i < prime; i++){
     if (prime % i === 0){
-      return 'is not a prime!'; //if divisible by 0, return false
+      return 'not prime'; //if divisible by 0, return false
     }
   }
-  return 'is a prime number!'; //if prime return
+  return 'is a prime'; //if prime return
 };
 
 

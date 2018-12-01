a38aaa2b513fddbdcc16d90f2abe7e7f52d8014f
DJMF <djm.fulton@gmail.com>
2018-12-01 19:57:38 +0000

    Update beers.js

--- week7/hw_pubsub_requests/hw_pubsub_api_requests/src/models/beers.js
+++ week7/hw_pubsub_requests/hw_pubsub_api_requests/src/models/beers.js
@@ -1,21 +1,24 @@
 const RequestHelper = require('../helpers/request_helper.js');
 const PubSub = require('../helpers/pub_sub.js');
 
-//constructor
-const headlines = function () {
+//constructor should be Beers at that what you're exporting
+const Beers = function () {
   this.beerData = [];
 };
 
 Beers.prototype.bindEvents = function () {
-  PubSub.subscribe('Beers:SelectView:beers-selected', event => {
+  // was missing brackets around event on next line
+  PubSub.subscribe('Beers:SelectView:beers-selected', (event) => {
     const selectedBeers =  event.detail;
-    const nameInChosenBeer = this.getListOfBeers(selectedBeers)
+    const nameInChosenBeer = this.getListOfBeers(selectedBeers);
     PubSub.publish('Beers:names-ready', nameInChosenBeer);
+  });
+};
 
 Beers.prototype.getBeers = function () {
   const requestHelper = new requestHelper('https://api.punkapi.com/v2/beers');
 //console.log(requestHelper);
-  requestHelper.get().then(beers) => {
+  requestHelper.get().then((beers) => { // should be .then((beers) => {})
     this.beerData = beers;
     const name = this.getListofNames();
 
@@ -28,11 +31,51 @@ Beers.prototype.getBeers = function () {
 Beers.prototype.getListofNames = function () {
   return this.beerData
   .map(brew => brew.name)
-  .filter((beer, index, beers) => beers.indexOf(beer) === index;
+  .filter((beer, index, beers) => beers.indexOf(beer) === index);
 };
 
 Beers.prototype.getListOfBeers = function (name) {
+  // was missing bracket end of next line
   return this.beerData.filter(brew => brew.name === name);
 };
 
 module.exports = Beers;
+
+// const RequestHelper = require('../helpers/request_helper.js');
+// const PubSub = require('../helpers/pub_sub.js');
+
+// //constructor
+// const headlines = function () {
+//   this.beerData = [];
+// };
+
+// Beers.prototype.bindEvents = function () {
+//   PubSub.subscribe('Beers:SelectView:beers-selected', event => {
+//     const selectedBeers =  event.detail;
+//     const nameInChosenBeer = this.getListOfBeers(selectedBeers)
+//     PubSub.publish('Beers:names-ready', nameInChosenBeer);
+
+// Beers.prototype.getBeers = function () {
+//   const requestHelper = new requestHelper('https://api.punkapi.com/v2/beers');
+// //console.log(requestHelper);
+//   requestHelper.get().then(beers) => {
+//     this.beerData = beers;
+//     const name = this.getListofNames();
+
+//      console.log(name);
+//     PubSub.publish('Beers:beers-ready', name);
+//     PubSub.publish('Beers:names-ready', this.beerData);
+//   });
+// };
+
+// Beers.prototype.getListofNames = function () {
+//   return this.beerData
+//   .map(brew => brew.name)
+//   .filter((beer, index, beers) => beers.indexOf(beer) === index;
+// };
+
+// Beers.prototype.getListOfBeers = function (name) {
+//   return this.beerData.filter(brew => brew.name === name);
+// };
+
+// module.exports = Beers;

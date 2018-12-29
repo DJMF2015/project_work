e4e5a810b675a37416f72bb8439584102564224c
DJMF2015 <djm.fulton@gmail.com>
2018-12-29 18:20:40 +0000

    completio of  isogram task

--- week6/day3/hw_advanced_enumeration_start/isogram_finder/isogram_finder.js
+++ week6/day3/hw_advanced_enumeration_start/isogram_finder/isogram_finder.js
@@ -1,5 +1,5 @@
 const IsogramFinder = function (words) {
-  this.words = words.toLowerCase();
+  this.words = words.toLowerCase()
 }
 //split each letter from words
 //use every method to return the values based upon ondition
@@ -7,8 +7,10 @@ const IsogramFinder = function (words) {
 //in helper method select unique letters by use of indexOF and lastIndexOF to get last element or -1
 
 IsogramFinder.prototype.isIsogram = function () {
-
-
+  return this.words.split('').every(character => this.isUnique(character));
 };
 
+IsogramFinder.prototype.isUnique = function (char) {
+return this.words.indexOf(char) ===  this.words.lastIndexOf(char) 
+};
 module.exports = IsogramFinder;
